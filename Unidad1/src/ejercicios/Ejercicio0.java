55282b9980796e7a2c89a92f3347ee3b819a7d84
rmmateosv <rmmateosv@gmail.com>
2020-09-17 14:26:34 +0200

    17/09

--- Unidad1/src/ejercicios/Ejercicio0.java
+++ Unidad1/src/ejercicios/Ejercicio0.java
@@ -1,6 +1,8 @@
 package ejercicios;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Ejercicio0 {
@@ -27,8 +29,10 @@ public class Ejercicio0 {
 					ejercicio1();
 					break;
 				case 2:
+					ejercicio2();
 					break;
 				case 3:
+					ejercicio3();
 					break;
 				case 4:
 					break;
@@ -40,6 +44,70 @@ public class Ejercicio0 {
 			
 		}while(opcion!=0);
 	}
+	private static void ejercicio3() {
+		// TODO Auto-generated method stub
+		//Pedir nombre fichero o carpeta
+		System.out.println("Introduce nombre de fichero o carpeta");
+		String nombre = t.nextLine();
+		
+		//Creamos objeto File para acceder a su información
+		File fichero  = new File(nombre);
+		
+		//comprobamos si existe
+		if(fichero.exists()) {
+			//Comprobamos si es carpeta o fichero
+			if(fichero.isDirectory()) {
+				//Accedemos a su cotenido
+				File[] contenido= fichero.listFiles();
+				//Recorremos contenido para mostra la información
+				for(int i=0;i<contenido.length;i++) {
+					
+				}
+			}
+			else {
+				System.out.println("El nombre corresponde a un fichero");
+			}
+			
+			
+		}
+		else {
+			System.out.println("El nombre no existe");
+		}
+	}
+
+	private static void ejercicio2() {
+		// TODO Auto-generated method stub
+		//Pedir nombre fichero o carpeta
+		System.out.println("Introduce nombre de fichero o carpeta");
+		String nombre = t.nextLine();
+		
+		//Creamos objeto File para acceder a su información
+		File fichero  = new File(nombre);
+		
+		//comprobamos si existe
+		if(fichero.exists()) {
+			//Comprobamos si es carpeta o fichero
+			if(fichero.isDirectory()) {
+				System.out.println("Carpeta");
+			}
+			else {
+				System.out.println("Fichero");
+			}
+			
+			//Mostramos fecha
+			//Crear un formato de fecha
+			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+			String fecha = formato.format(new Date(fichero.lastModified()));
+			
+			System.out.println("Fecha última modificación:"+ fecha);
+			
+			//Mostramos el tamaño
+			System.out.println("Tamaño:"+fichero.length());
+		}
+		else {
+			System.out.println("El nombre no existe");
+		}
+	}
 
 	private static void ejercicio1() {
 		// TODO Auto-generated method stub
@@ -49,6 +117,8 @@ public class Ejercicio0 {
 		//Declaramos objeto File
 		//. es la carpeta actual
 		File fichero = new File(".");
+		//Mostramos la ruta absoluta
+		System.out.println("Ruta Absoluta:" + fichero.getAbsolutePath());
 		
 	}
 
