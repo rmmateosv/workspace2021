872b2e88178110c04ca6ee714f1cd34c51bc3716
rmmateosv <rmmateosv@gmail.com>
2020-10-06 13:46:42 +0200

    06/10_2

--- Unidad1/src/cooperativaControlador/PrincipalEntregas.java
+++ Unidad1/src/cooperativaControlador/PrincipalEntregas.java
@@ -3,18 +3,26 @@ package cooperativaControlador;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
+import cooperativaModelo.Entregas;
 import cooperativaModelo.FicheroEntregas;
 import cooperativaModelo.FicheroFrutas;
 import cooperativaModelo.FicheroSocios;
 import cooperativaModelo.Frutas;
 import cooperativaModelo.Socio;
 
+
+
+
+
 public class PrincipalEntregas {
 
 	private static Scanner t = new java.util.Scanner(System.in); 
 	private static FicheroEntregas fe = new FicheroEntregas("entregas.txt");
+	private static FicheroSocios fs = new FicheroSocios("socios.txt");
+	private static FicheroFrutas ff = new FicheroFrutas("frutas.txt");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -31,9 +39,41 @@ public class PrincipalEntregas {
 			
 			
 			opcion = t.nextInt();t.nextLine();
-			Frutas f;
+			Entregas e;
 			switch(opcion){
-				case 1:			
+				case 1:
+					e = new Entregas();
+					ArrayList<Socio> lS = fs.obtenerSocios();
+					for(Socio s:lS) {
+						s.mostrar();
+					}
+					System.out.println("Introduce el Nif del Socio:");
+					e.setSocio(fs.obtenerSocio(t.nextLine()));
+					if(e.getSocio()!=null) {
+						//Mostramos frutas
+						ArrayList<Frutas> lF = ff.obtenerFrutas();
+						for(Frutas f:lF) {
+							f.mostrar();
+						}
+						System.out.println("Introduce el código de fruta");
+						e.setFruta(ff.obtenerFruta(t.nextInt())); t.nextLine();
+						if(e.getFruta()!=null) {
+							e.setFecha(new Date());
+							System.out.println("Introduce kilos");
+							e.setKilos(t.nextFloat()); t.nextLine();
+							System.out.println("Introduce precio Kg");
+							e.setPrecio(t.nextFloat()); t.nextLine();
+							if(!fe.crearEntrega(e)) {
+								System.out.println("Error al crear la entrega");
+							}
+						}
+						else {
+							System.out.println("Error: La fruta no existe");
+						}
+					}
+					else {
+						System.out.println("Error: El socio no existe");
+					}
 					
 					break;
 				case 2:
--- Unidad1/src/cooperativaModelo/FicheroEntregas.java
+++ Unidad1/src/cooperativaModelo/FicheroEntregas.java
@@ -1,4 +1,10 @@
 package cooperativaModelo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 //ESTRUCTURA DEL FICHERO DE ENTREGAS
 //Codigo int => 4B
 //NIF Socio String de 9 caracteres=> 18B
@@ -21,6 +27,64 @@ public class FicheroEntregas {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public boolean crearEntrega(Entregas e) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		
+		
+		//Declaramos fichero de acceso aleatorio
+		RandomAccessFile fichero = null;
+		
+		//Abrimos el fichero para leer y escribir
+		try {
+			
+			fichero = new RandomAccessFile(nombre, "rw");
+			
+			//Obtenemos el código de la última entrega
+			//Paso 1: Nos movemos al final del fichero
+			//Nos ponemos al principio del último registro si hay registros
+			if(fichero.length()==0) {
+				e.setCodigo(1);
+			}
+			else {
+				fichero.seek(fichero.length()-42);
+				//Leemos el código del último registro
+				int codigo = fichero.readInt();
+				e.setCodigo(codigo+1);
+			}
+					
+			//Nos ponemos al final del fichero para guardar la nueva entrega
+			fichero.seek(fichero.length());
+			fichero.writeInt(e.getCodigo());
+			//Hacemos que el dni tenga longitud fija de 9
+			StringBuffer dni = new StringBuffer(e.getSocio().getNif());
+			dni.setLength(9);
+			fichero.writeChars(dni.toString());
+			fichero.writeInt(e.getFruta().getCodigo());
+			fichero.writeLong(e.getFecha().getTime());
+			fichero.writeFloat(e.getKilos());
+			fichero.writeFloat(e.getPrecio());
+			resultado=true;	
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Fichero aún no existe");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		finally {
+			if(fichero!=null) {
+				try {
+					fichero.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+		return resultado;
 	}
 	
 	
