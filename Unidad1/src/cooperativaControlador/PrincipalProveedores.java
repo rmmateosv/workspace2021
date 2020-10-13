0afd720dcf8d43553824b45c49f844f71731c79d
rmmateosv <rmmateosv@gmail.com>
2020-10-13 14:31:52 +0200

    13/10/20

--- Unidad1/src/cooperativaControlador/PrincipalProveedores.java
+++ Unidad1/src/cooperativaControlador/PrincipalProveedores.java
@@ -6,53 +6,82 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
-
 import cooperativaModelo.FicheroProveedores;
 import cooperativaModelo.Proveedor;
 
-
-
-
-
 public class PrincipalProveedores {
 
-	private static Scanner t = new java.util.Scanner(System.in); 
-	private static FicheroProveedores fp = 
-			new FicheroProveedores("proveedores.xml");
-	
+	private static Scanner t = new java.util.Scanner(System.in);
+	private static FicheroProveedores fp = new FicheroProveedores("proveedores.xml");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int opcion = 0;
 		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		do {
 			System.out.println("Introduce una opción");
 			System.out.println("0-Salir");
 			System.out.println("1-Alta Proveedor");
 			System.out.println("2-Mostrar Proveedores");
 			System.out.println("3-Modificar fecha último pedido");
-			System.out.println("3-Borrar Proveedores "
-					+ "(crea fichero con los que están de baja)!!!!!");
-			
-			
-			opcion = t.nextInt();t.nextLine();
-			
-			
-			switch(opcion){
-				case 1:
-					
-					break;
-				case 2:
-					ArrayList<Proveedor> listaP=fp.obtenerProveedores();
-					for(Proveedor p : listaP) {
-						p.mostrar();
+			System.out.println("3-Borrar Proveedores " + "(crea fichero con los que están de baja)!!!!!");
+
+			opcion = t.nextInt();
+			t.nextLine();
+
+			Proveedor pr;
+			switch (opcion) {
+			case 1:
+				try {
+					pr = new Proveedor();
+					System.out.println("Introduce el nombre");
+					pr.setNombre(t.nextLine());
+					System.out.println("Teléfono");
+					pr.getTelefonos().add(t.nextLine());
+					pr.setFecha_pedido(formato.parse("01/01/1900"));
+					pr.setBaja(false);
+
+					pr.setCodigo(fp.obtenerNuevoCodigo());
+
+					if (!fp.altaProveedor(pr)) {
+						System.out.println("Error al dar de alta el proveedor");
 					}
-					break;
-				
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				ArrayList<Proveedor> listaP = fp.obtenerProveedores();
+				for (Proveedor p : listaP) {
+					p.mostrar();
+				}
+				break;
+			case 3:
+
+				try {
+					System.out.println("Código proveedor:");
+					int codigo = t.nextInt();
+					t.nextLine();
+					pr = fp.obtenerProveedor(codigo);
+					if (pr != null) {
+						System.out.println("Nueva fecha (dd/mm/yyyy)");
+						pr.setFecha_pedido(formato.parse(t.nextLine()));
+						pr.mostrar();
+						//fp.modificarFecha(pr);
+					} else {
+						System.out.println("Error, el proveedor no existe");
+					}
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+
 			}
-			
-		}while(opcion!=0);
+
+		} while (opcion != 0);
 	}
 
 }
--- Unidad1/src/cooperativaModelo/FicheroProveedores.java
+++ Unidad1/src/cooperativaModelo/FicheroProveedores.java
@@ -1,5 +1,6 @@
 package cooperativaModelo;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,12 +9,23 @@ import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 public class FicheroProveedores {
@@ -88,6 +100,204 @@ public class FicheroProveedores {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return resultado;
+	}
+
+	public int obtenerNuevoCodigo() {
+		// TODO Auto-generated method stub
+		int resultado = 0;
+		
+		File fichero = new File(nombre);
+		if(!fichero.exists()) {
+			resultado = 1;
+		}
+		else {			
+			try {
+				//Cargamos el fichero en un árbol DOM
+				Document documento = DocumentBuilderFactory.
+						newInstance().newDocumentBuilder().parse(nombre);
+				//Obtenemos raíz
+				Element raiz = documento.getDocumentElement();
+				//Obtenemos el último nodo provedor
+				Element ultimo = (Element) raiz.getLastChild();
+				resultado = Integer.parseInt(ultimo.getAttribute("id"))+1;
+				
+			
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return resultado;
+	}
+
+	public boolean altaProveedor(Proveedor pr) {
+		// TODO Auto-generated method stub
+		boolean resultado = false;
+		try {
+			Document documento = DocumentBuilderFactory.
+					newInstance().newDocumentBuilder().parse(nombre);
+			Element raiz = documento.getDocumentElement();
+			
+			//Creamos el nodo elemento proveedor
+			Element proveedor = documento.createElement("proveedor");
+			//Añadimos proveedor a la raiz
+			raiz.appendChild(proveedor);
+			//Rellenamos el atributo id de proveedor
+			proveedor.setAttribute("id", String.valueOf(pr.getCodigo()));
+			
+			//Nombre
+			Element nombre = documento.createElement("nombre");
+			proveedor.appendChild(nombre);
+			//Nodo Text con el valor del nombre
+			Text textoNombre = documento.createTextNode(pr.getNombre());
+			nombre.appendChild(textoNombre);
+			
+			//Teléfonos
+			Element telefonos = documento.createElement("telefonos");
+			proveedor.appendChild(telefonos);
+			
+			//Teléfono
+			Element telefono = documento.createElement("telefono");
+			telefonos.appendChild(telefono);
+			//Texto con el tlf
+			Text textoTelf = documento.createTextNode(pr.getTelefonos().get(0));
+			telefono.appendChild(textoTelf);
+			
+			//Fecha
+			Element fecha = documento.createElement("fecha_pedido");
+			proveedor.appendChild(fecha);
+			//Nodo Text con el valor de la fecha
+			Text textoFecha = documento.createTextNode(formato.format(pr.getFecha_pedido()));
+			fecha.appendChild(textoFecha);
+			
+			//Baja
+			Element baja = documento.createElement("baja");
+			proveedor.appendChild(baja);
+			//Nodo Text con el valor baja
+			Text textoBaja = documento.createTextNode(Boolean.toString(pr.isBaja()));
+			baja.appendChild(textoBaja);
+			
+			//Guardar el DOM en un fichero
+			guardarDOM(documento);
+			
+			resultado = true;
+			
+			
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return resultado;
+	}
+
+	private void guardarDOM(Document documento) {
+		// TODO Auto-generated method stub
+		
+		try {
+			//Establecemos el árbol a guardar
+			Source fuente = new DOMSource(documento);
+			//Establecemos el fichero destion
+			Result destino = new StreamResult(new File(nombre));
+			//Creamos el transformador
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			//Creamos el nuevo fichero
+			t.transform(fuente, destino);
+			
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public Proveedor obtenerProveedor(int codigo) {
+		// TODO Auto-generated method stub
+		Proveedor resultado = null;
+		
+			Document documento;
+			try {
+				documento = DocumentBuilderFactory.
+						newInstance().newDocumentBuilder().parse(nombre);
+				Element raiz = documento.getDocumentElement();
+				//Obtenemos los elementos proveedor
+				NodeList proveedores = raiz.getChildNodes();
+				for(int i=0;i<proveedores.getLength();i++) {
+					//comprobamos si el código es el buscado
+					int codigoXML = Integer.parseInt(
+							proveedores.item(i)
+							.getAttributes().item(0)
+							.getNodeValue());
+					if(codigoXML == codigo) {
+						resultado = new Proveedor();
+						resultado.setCodigo(codigoXML);
+						//Nombre
+						resultado.setNombre(proveedores.item(i)
+								.getChildNodes().item(0)
+								.getChildNodes().item(0).getNodeValue());
+						//Telefonos
+						for(int j=0;j<proveedores.item(i)
+								.getChildNodes().item(1)
+								.getChildNodes().getLength();j++) {
+							resultado.getTelefonos().add(proveedores.item(i)
+									.getChildNodes().item(1)
+									.getChildNodes().item(j)
+									.getChildNodes().item(0) //!!!!
+									.getNodeValue());
+						}
+						//Fecha
+						resultado.setFecha_pedido(formato.parse(proveedores.item(i)
+								.getChildNodes().item(2)
+								.getChildNodes().item(0).getNodeValue())
+								);
+						//Baja
+						resultado.setBaja(Boolean.parseBoolean(proveedores.item(i)
+								.getChildNodes().item(3)
+								.getChildNodes().item(0).getNodeValue()));
+						
+						return resultado;
+						
+					}
+				}
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (DOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		
 		return resultado;
 	}
 	
