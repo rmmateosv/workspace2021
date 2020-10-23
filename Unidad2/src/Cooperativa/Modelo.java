66f30cd8fb8893c0b2bb53553478f2aa3ee476c2
rmmateosv <rmmateosv@gmail.com>
2020-10-23 12:08:05 +0200

    23_10_3

--- Unidad2/src/Cooperativa/Modelo.java
+++ Unidad2/src/Cooperativa/Modelo.java
@@ -2,7 +2,10 @@ package Cooperativa;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Modelo {
 	
@@ -43,6 +46,38 @@ public class Modelo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public ArrayList<Socio> obtenerSocios() {
+		// TODO Auto-generated method stub
+		ArrayList<Socio> resultado  = new ArrayList<>();
+		
+		try {
+			//Declaramos la sentencia a ejecutar
+			Statement sentencia = conexion.createStatement();
+			//Ejecutamos la sentencia y guardamos el resultado en r
+			ResultSet r = sentencia.executeQuery("select * from socio");
+			//Recorremos r, es una lista con los socios que devuelve el select
+			while(r.next()) {
+				Socio s = new Socio();
+				//Recuperamos el dni como un string que está en la posición 1
+				//La primera posción es la 1 NO LA 0!!
+				s.setNif(r.getString(1));
+				//Recuperomos el nombre como un string cuyo campo en la tabla se llama nombre.
+				//Es lo mismo usar posición que nombre de campo 
+				s.setNombre(r.getString("nombre"));
+				s.setFechAlta(r.getDate(3));
+				s.setSaldo(r.getFloat("saldo"));
+				s.setBaja(r.getBoolean(5));
+				
+				resultado.add(s);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return resultado;
 	}
 			
 }
--- Unidad2/src/Cooperativa/Principal.java
+++ Unidad2/src/Cooperativa/Principal.java
@@ -8,35 +8,44 @@ import java.util.Scanner;
 public class Principal {
 
 	private static Scanner t = new java.util.Scanner(System.in); 
-	private Modelo bd = new Modelo();
+	private static Modelo bd = new Modelo();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int opcion = 0;
-		
-		do {
-			System.out.println("Introduce una opción");
-			System.out.println("0-Salir");
-			System.out.println("1-Mostrar Socios");			
-			
-						
-			switch(opcion){
-				case 1:
-							
-					break;
-				case 2:
-					
-					break;
-				case 3:
-					
-					break;
-				case 4:
-									
-					break;
+		if(bd.getConexion()!=null) {
+			do {
+				System.out.println("Introduce una opción");
+				System.out.println("0-Salir");
+				System.out.println("1-Mostrar Socios");			
 				
-			}
-			
-		}while(opcion!=0);
+				opcion = t.nextInt(); t.nextLine();			
+				switch(opcion){
+					case 1:
+						ArrayList<Socio> socios = bd.obtenerSocios();
+						for(Socio s:socios) {
+							s.mostrar();
+						}
+						break;
+					case 2:
+						
+						break;
+					case 3:
+						
+						break;
+					case 4:
+										
+						break;
+					
+				}
+				
+			}while(opcion!=0);
+			//Cerramos la conexión con la BD.
+			bd.cerrar();
+		}
+		else {
+			System.out.println("No se ha podido conectar con la BD");
+		}
 	}
 
 }
