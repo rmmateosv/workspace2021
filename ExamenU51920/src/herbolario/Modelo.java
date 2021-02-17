3f85ab7a852ff18ec7c8341831c29545d413919e
rmmateosv <rmmateosv@gmail.com>
2021-02-17 10:07:32 +0100

    17_02_3

--- ExamenU51920/src/herbolario/Modelo.java
+++ ExamenU51920/src/herbolario/Modelo.java
@@ -91,5 +91,94 @@ public class Modelo {
 		}
 		return resultado;
 	}
+	public Producto obtenerProducto(int codigo) {
+		// TODO Auto-generated method stub
+		Producto  resultado = null;
+		try {
+			PreparedStatement sentencia = conexion.prepareStatement("select codigo, nombre, "
+					+ "(info).kcal, "
+					+ "(info).grasa, (info).hidratos, precios from producto "
+					+ "where codigo = ?");
+			sentencia.setInt(1, codigo);
+			ResultSet rs = sentencia.executeQuery();
+			if(rs.next()) {
+				Producto p = new Producto();
+				p.setCodigo(rs.getInt(1));
+				p.setNombre(rs.getString(2));
+				p.setInfo(new DatosNutricion(rs.getInt(3),rs.getInt(4),rs.getInt(5)));
+				//Pasar el array de la bd al array list de la clase
+				Array arrayPrecios = rs.getArray(6);
+				Integer[] precios = (Integer[]) arrayPrecios.getArray();
+				for(int i=0;i<precios.length;i++) {
+					p.getPrecios().add(precios[i]);
+				}
+				resultado = p;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+	public boolean addPrecio(Producto p, int precio) {
+		// TODO Auto-generated method stub
+		boolean  resultado = false;
+		try {
+			PreparedStatement sentencia = conexion.prepareStatement("update producto "
+					+ "set precios = array_cat(precios, array[?]::integer[]) "
+					+ "where codigo = ?");
+			sentencia.setInt(1, precio);
+			sentencia.setInt(2, p.getCodigo());
+			int ok = sentencia.executeUpdate();
+			if(ok==1) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+	public boolean crearVenta(Venta v) {
+		// TODO Auto-generated method stub
+		boolean  resultado = false;
+		try {
+			PreparedStatement sentencia = conexion.prepareStatement("insert into venta "
+					+ "values (default, ?,?,?,?)");
+			sentencia.setDate(1, new java.sql.Date(v.getFecha().getTime()));
+			sentencia.setInt(2, v.getProducto().getCodigo());
+			sentencia.setInt(3, v.getCantidad());
+			sentencia.setInt(4, v.getPrecio());
+			int ok = sentencia.executeUpdate();
+			if(ok==1) {
+				resultado = true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+	public ArrayList<Object[]> obtenerEstadistica() {
+		// TODO Auto-generated method stub
+		ArrayList<Object[]> resultado = new ArrayList();
+		try {
+			Statement sentencia = conexion.createStatement();
+			ResultSet rs = sentencia.executeQuery("select producto, sum(cantidad), sum(precio) "
+					+ "from venta "
+					+ "group by producto");
+			while(rs.next()) {
+				Object [] datos = new Object[3];
+				datos[0] = rs.getInt(1);
+				datos[1] = rs.getInt(2);
+				datos[2] = rs.getInt(3);
+				resultado.add(datos);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultado;
+	}
 	
 }
--- ExamenU51920/src/herbolario/Principal.java
+++ ExamenU51920/src/herbolario/Principal.java
@@ -3,6 +3,7 @@ package herbolario;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Principal {
@@ -38,10 +39,10 @@ public class Principal {
 						crearPrecio();
 						break;
 					case 3:
-						
+						crearVenta();
 						break;
 					case 4:
-						
+						estadistica();
 						break;
 					case 5:
 						
@@ -64,6 +65,42 @@ public class Principal {
 
 
 }
+	private static void estadistica() {
+		// TODO Auto-generated method stub
+		ArrayList<Object[]> datos = bd.obtenerEstadistica();
+		for(Object[] o: datos) {
+			System.out.println("Producto:"+ o[0] + 
+					"\tUdesVendidas:" + o[1] +
+					"\tImporteToal:" + o[2]);
+		}
+	}
+	private static void crearVenta() {
+		// TODO Auto-generated method stub
+		mostrarProductos();
+		System.out.println("Introduce código");
+		int codigo = t.nextInt(); t.nextLine();
+		Producto p = bd.obtenerProducto(codigo);
+		if(p!=null) {
+			if(p.getPrecios().size()==0) {				
+				System.out.println("No se puede crear venta porque no hay precio");
+			}
+			else {
+				Venta v = new Venta();
+				v.setProducto(p);
+				v.setFecha(new Date());
+				System.out.println("Cantidad");
+				v.setCantidad(t.nextInt()); t.nextLine();
+				v.setPrecio(v.getCantidad()*p.getPrecios().get(p.getPrecios().size()-1));
+				if(!bd.crearVenta(v)) {
+					System.out.println("Error al crear la venta");
+				}
+			}
+		
+		}
+		else {
+		System.out.println("Error, producto no existe");
+		}
+	}
 	private static void crearPrecio() {
 		// TODO Auto-generated method stub
 		mostrarProductos();
